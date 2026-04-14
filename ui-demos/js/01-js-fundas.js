851adb0568a8c957d96112b4e099d55f1ba7d51d
Vaman Deshmukh <vamandeshmukh@gmail.com>
2026-04-14 14:49:50 +0530

    js fundas

--- ui-demos/js/01-js-fundas.js
+++ ui-demos/js/01-js-fundas.js
@@ -1,87 +1,93 @@
-// console.log("JavaScript fundamentals");
-// console.log("=======================");
+// // console.log("JavaScript fundamentals");
+// // console.log("=======================");
 
-// console.log("Variable declaration in JS");
+// // console.log("Variable declaration in JS");
 
-// // Java 
-// // int num = 10;
+// // // Java 
+// // // int num = 10;
 
-// // JavaScript
-// // num1 = 10; // do not use this 
-// // var num2 = 20; // do not use this too 
+// // // JavaScript
+// // // num1 = 10; // do not use this 
+// // // var num2 = 20; // do not use this too 
 
-// const num3 = 30; // default choice to declare variable 
-// let num4 = 40; // to declare "variable" variables
+// // const num3 = 30; // default choice to declare variable 
+// // let num4 = 40; // to declare "variable" variables
 
-// console.log(num1);
-// console.log(num2);
-// console.log(num3);
-// console.log(num4);
-// // num3 = 35; // error 
-// num4 = 45;
-// console.log(num3);
-// console.log(num4);
+// // console.log(num1);
+// // console.log(num2);
+// // console.log(num3);
+// // console.log(num4);
+// // // num3 = 35; // error 
+// // num4 = 45;
+// // console.log(num3);
+// // console.log(num4);
 
-// console.log("JavaScript - dynamically typed language");
-// console.log("---------------------------------------");
-// console.log("datatypes in JS - number, string, boolean, undefined, NaN ...");  
+// // console.log("JavaScript - dynamically typed language");
+// // console.log("---------------------------------------");
+// // console.log("datatypes in JS - number, string, boolean, undefined, NaN ...");  
 
-// let myVariable;
-// console.log(myVariable);
-// console.log(typeof myVariable);
-// myVariable = 10;
-// console.log(myVariable);
-// console.log(typeof myVariable);
-// myVariable = 20;
-// console.log(myVariable);
-// console.log(typeof myVariable);
-// myVariable = -15;
-// console.log(myVariable);
-// console.log(typeof myVariable);
-// myVariable = 10.50;
-// console.log(myVariable);
-// console.log(typeof myVariable);
-// myVariable = "abc";
-// console.log(myVariable);
-// console.log(typeof myVariable);
-// myVariable = false;
-// console.log(myVariable);
-// console.log(typeof myVariable);
+// // let myVariable;
+// // console.log(myVariable);
+// // console.log(typeof myVariable);
+// // myVariable = 10;
+// // console.log(myVariable);
+// // console.log(typeof myVariable);
+// // myVariable = 20;
+// // console.log(myVariable);
+// // console.log(typeof myVariable);
+// // myVariable = -15;
+// // console.log(myVariable);
+// // console.log(typeof myVariable);
+// // myVariable = 10.50;
+// // console.log(myVariable);
+// // console.log(typeof myVariable);
+// // myVariable = "abc";
+// // console.log(myVariable);
+// // console.log(typeof myVariable);
+// // myVariable = false;
+// // console.log(myVariable);
+// // console.log(typeof myVariable);
 
 
-// let field1 = 10;
-// // let field2 = 20;
-// // let field2 = "20";
-// let field2 = "a";
-// console.log(field1 + field2);
-// console.log(field1 - field2);
-// console.log(field1 * field2);
-// console.log(field1 / field2);
-// console.log(field1 % field2);
+// // let field1 = 10;
+// // // let field2 = 20;
+// // // let field2 = "20";
+// // let field2 = "a";
+// // console.log(field1 + field2);
+// // console.log(field1 - field2);
+// // console.log(field1 * field2);
+// // console.log(field1 / field2);
+// // console.log(field1 % field2);
+
+// let x = 10;
+// let y;
+// console.log(x + y); // NaN 
+// console.log(x - y); // NaN 
+
+// console.log("Truthy and falsy values in JS");
+// console.log('Falsy values - undefined, NaN, null, 0, "", false');
+// console.log('Truthy values - everything else');
+
+// let input = 1;
+
+// // if input is given, print 'yes' else print 'no'
+
+// if (input) {
+//     console.log("Yes");
+// }
+// else {
+//     console.log("No");
+// }
+
+console.log("special comparison operators === and !==");
 
 let x = 10;
-let y;
-console.log(x + y); // NaN 
-console.log(x - y); // NaN 
+let y = "10";
 
-console.log("Truthy and falsy values in JS");
-console.log('Falsy values - undefined, NaN, null, 0, "", false');
-console.log('Truthy values - everything else');
-
-
-let input = 1;
-
-// if input is given, print 'yes' else print 'no'
-
-if (input) {
-    console.log("Yes");
-}
-else {
-    console.log("No");
-}
-
-
-
+console.log(x == y);
+console.log(x === y);
+console.log(x != y);
+console.log(x !== y);
 
 
 
