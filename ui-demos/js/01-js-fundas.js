a0a1bc650e2ce492a270ee475ab842fb87a3d827
Vaman Deshmukh <vamandeshmukh@gmail.com>
2026-04-14 14:07:07 +0530

    js fundas

--- ui-demos/js/01-js-fundas.js
+++ ui-demos/js/01-js-fundas.js
@@ -7,8 +7,8 @@ console.log("Variable declaration in JS");
 // int num = 10;
 
 // JavaScript
-// num1 = 10; // do not follow this 
-// var num2 = 20; // do not follow this too 
+// num1 = 10; // do not use this 
+// var num2 = 20; // do not use this too 
 
 const num3 = 30; // default choice to declare variable 
 let num4 = 40; // to declare "variable" variables
@@ -17,7 +17,7 @@ let num4 = 40; // to declare "variable" variables
 // console.log(num2);
 console.log(num3);
 console.log(num4);
-console.log(this.CSS);
+
 
 
 
