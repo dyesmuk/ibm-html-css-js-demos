4f2c317c6e01d93cd9f91f172aabbea2a99072c8
Vaman Deshmukh <vamandeshmukh@gmail.com>
2026-04-14 12:46:28 +0530

    JavaScript fundas

---
+++ ui-demos/js/01-js-fundas.js
@@ -0,0 +1,11 @@
+console.log("JavaScript fundamentals");
+console.log("=======================");
+
+console.log("Variable declaration in JS");
+
+// Java 
+// int num = 10;
+// JavaScript
+const num2 = 20;
+
+console.log(num2);
---
+++ ui-demos/js/script.js
@@ -0,0 +1,5 @@
+console.log("JavaScript Demo");
+// 
+// datatyes, variable declaration, operators, objects, functions, OOP etc 
+// 
+//
