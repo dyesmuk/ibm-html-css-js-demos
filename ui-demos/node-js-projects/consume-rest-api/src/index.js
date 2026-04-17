ac1ac2d81ac836ad21b3a954c8a8d4a9c42c338f
Vaman Deshmukh <vamandeshmukh@gmail.com>
2026-04-17 12:06:35 +0530

    consume rest apis

---
+++ ui-demos/node-js-projects/consume-rest-api/src/index.js
@@ -0,0 +1,44 @@
+// index.js 
+
+console.log("Consume REST APIs");
+
+import axios from 'axios';
+
+const BASE_URL = 'http://localhost:8090';
+
+const api = axios.create({
+    baseURL: BASE_URL
+});
+
+const login = async () => {
+    try {
+        const params = new URLSearchParams();
+        params.append('username', 'sonu');
+        params.append('password', 'sonu');
+
+        const response = await axios.post(`${BASE_URL}/login`, params);
+        console.log(response.data);
+        const token = response.data.token || response.data.jwtToken || response.data;
+        console.log(token);
+        api.defaults.headers.common['Authorization'] = `Bearer ${token}`;
+    } catch (error) {
+        console.error(error);
+    }
+};
+
+const getEmployees = async () => {
+    try {
+        const response = await api.get('/api/v1/employees');
+        console.log('All Employees:', response.data[0]);
+    } catch (error) {
+        console.error(error);
+    }
+};
+
+
+const run = async () => {
+    await login();
+    await getEmployees();
+};
+
+run();
