72d9593464921eba19591c662465cee1ae177415
Vaman Deshmukh <vamandeshmukh@gmail.com>
2026-04-17 10:15:07 +0530

    nodejs project

--- ui-demos/node-js-demos/01-node-es6.js
+++ ui-demos/node-js-demos/01-node-es6.js
@@ -39,14 +39,36 @@ console.log("ES6 Concepts");
 // addNums([10, 20, 30, 40]);
 
 // option 2 = use rest operator  ...
-const addNums = (...arr) => {
-    let sum = 0;
-    for (let a of arr) {
-        sum += a;
-    }
-    console.log(sum);
-};
-addNums(10, 20);
-addNums(10, 20, 30);
-addNums(10, 20, 30, 40);
-addNums(10, 20, 30, 40, 50);
+// rest -> collect and pack 
+// spread -> destructure or unpack 
+
+// const addNums = (...arr) => {
+//     let sum = 0;
+//     for (let a of arr) {
+//         sum += a;
+//     }
+//     console.log(sum);
+// };
+// addNums(10, 20);
+// addNums(10, 20, 30);
+// addNums(10, 20, 30, 40);
+// addNums(10, 20, 30, 40, 50);
+
+const friends = ["Sona", "Sonu", "Monu", "Tonu", "Ponu"];
+console.log(friends);
+
+[besty, ...closeFriends] = friends;
+
+console.log(besty); // Sona 
+console.log(closeFriends); // others 
+
+// spread - 
+const arr1 = [0, 1, 2];
+const arr2 = [3, 4, 5];
+const arr3 = [...arr1, ...arr2];
+console.log(arr3);
+
+
+
+
+
