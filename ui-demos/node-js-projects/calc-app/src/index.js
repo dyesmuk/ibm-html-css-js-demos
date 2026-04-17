d130fb50ef31ecec4ddf9dd80fecbb4cbeed07ac
Vaman Deshmukh <vamandeshmukh@gmail.com>
2026-04-17 10:59:05 +0530

    calc app project - imports and exports

--- ui-demos/node-js-projects/calc-app/src/index.js
+++ ui-demos/node-js-projects/calc-app/src/index.js
@@ -1,13 +1,22 @@
-// https://github.com/dyesmuk/ibm-html-css-js-demos/blob/main/courseware/03-js/09_Modules_ES6.md#exercises
+// index.js 
 
-// Create a math.js module that exports add, subtract, multiply, divide as named exports and a Calculator class as the default export.
+// https://github.com/dyesmuk/ibm-html-css-js-demos/blob/main/courseware/03-js/09_Modules_ES6.md#exercises 
+// Create a math.js module that exports add, subtract, multiply, divide as named exports and a Calculator class as the default export. 
 
 console.log("-- welcome to calc app --");
 
-addNums(10, 20);
 
-subNums(10, 5);
+// // 1. Named Export 
+// import { addNums, subNums, divNums, multiNums } from "./math.js";
 
-divNums(10, 5);
+// addNums(10, 20);
+// subNums(10, 5);
+// divNums(10, 5);
+// multiNums(10, 20);
 
-multiNums(10, 20);
+// // 2. Default Export 
+import math from "./math.js";
+math.addNums(10, 20);
+math.subNums(10, 5);
+math.divNums(10, 5);
+math.multiNums(10, 20);
--- ui-demos/node-js-projects/calc-app/src/math.js
+++ ui-demos/node-js-projects/calc-app/src/math.js
@@ -1,9 +1,38 @@
+// math.js
 
+// Types of exports and imports 
+// ----------------------------
 
-const addNums = (a, b) => { };
+// 1. Named Export 
+// Use {} while importing
+// Names must match exactly
+// Can export multiple things
+// Use when you have multiple utilities/functions
 
-const subNums = (a, b) => { };
+// // // variation 1 
+// const addNums = (a, b) => { console.log(a + b); };
+// const subNums = (a, b) => { console.log(a - b); };
+// const divNums = (a, b) => { console.log(a / b); };
+// const multiNums = (a, b) => { console.log(a * b); };
+// export { addNums, subNums, divNums, multiNums };
 
-const divNums = (a, b) => { };
+// // variation 2
+// export const addNums = (a, b) => { console.log(a + b); };
+// export const subNums = (a, b) => { console.log(a - b); };
+// export const divNums = (a, b) => { console.log(a / b); };
+// export const multiNums = (a, b) => { console.log(a * b); };
 
-const multiNums = (a, b) => { };
+// 2. Default Export
+// NO {} while importing
+// You can give any name during import
+// Only one default export per file
+// Use when file represents one main thing
+
+const math = {
+    addNums: (a, b) => { console.log(a + b); },
+    subNums: (a, b) => { console.log(a - b); },
+    divNums: (a, b) => { console.log(a / b); },
+    multiNums: (a, b) => { console.log(a * b); },
+};
+
+export default math;
