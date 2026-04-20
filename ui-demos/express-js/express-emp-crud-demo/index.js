d4e7f6593d4e5b1e80a946bcfe662bbe01908d12
Vaman Deshmukh <vamandeshmukh@gmail.com>
2026-04-20 14:35:04 +0530

    updated code

--- ui-demos/express-js/express-emp-crud-demo/index.js
+++ ui-demos/express-js/express-emp-crud-demo/index.js
@@ -3,10 +3,11 @@ import mongoose from "mongoose";
 import employeeRoutes from "./routes/employee.routes.js";
 
 const app = express();
+const PORT = 3000;
 app.use(express.json());
 
 // MongoDB connection
-mongoose.connect("mongodb://127.0.0.1:27017/employeeDB")
+mongoose.connect("mongodb://127.0.0.1:27017/ibm-ems")
   .then(() => console.log("MongoDB connected"))
   .catch(err => console.error(err));
 
@@ -14,6 +15,6 @@ mongoose.connect("mongodb://127.0.0.1:27017/employeeDB")
 app.use("/employees", employeeRoutes);
 
 // Server
-app.listen(3000, () => {
-  console.log("Server running on port 3000");
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}.`);
 });
